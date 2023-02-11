d3fe4e546731973c7b24a90909b2c78546f2b402
hamza herbou <hamza95herbou@gmail.com>
2023-02-11 19:17:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var st = require("node-static");
+var nodeStatic = require("node-static")
 
-var file = new st.Server("./public");
-const port = 9000;
+const PORT = 9000
+const folder = "./public"
+var file = new nodeStatic.Server(folder)
 
-require("http").createServer((req,res)=>{
-	req.addListener("end",()=>{
-		file.serve(req,res);
-	}).resume();
-}).listen(port);
 
-console.log(".  Listening on :  http://127.0.0.1:"+port+"/");
+require("http").createServer((req,res) => {
+	console.log(".    # Http Request.")
+	req.addListener("end",() => file.serve(req,res)).resume()
+}).listen(PORT)
+
+
+console.log(".    Listening on :  http://127.0.0.1:"+PORT+"/")
+console.log(".")
+console.log(".")
